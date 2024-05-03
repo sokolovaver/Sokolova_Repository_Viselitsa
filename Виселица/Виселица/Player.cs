6db11bc0899e15e343a5cf98cb1786bb3428407d
Sokolova Veronika Alekseevna <veronika.sock@yandex.ru>
2024-05-03 19:37:01 +0300

    [RP-2] Разработка класса Player

--- Виселица/Виселица/Player.cs
+++ Виселица/Виселица/Player.cs
@@ -12,6 +12,7 @@ namespace Виселица
     public partial class Player : Form
     {
 
+        Button b1 = new Button();
         public Player()
         {
             InitializeComponent();
@@ -20,20 +21,62 @@ namespace Виселица
         private void button4_Click(object sender, EventArgs e)
         {
 
+            Menu men = new Menu();
+            men.Show();//переход на форму
+            this.Hide();//закрытие предыдущей формы
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (textBox1.Text != "")
+            {
+                ScoreBoard.me = textBox1.Text;
+                DifficultyCountries dcount = new DifficultyCountries();
+                dcount.Show();//переход на форму
+                this.Hide();//закрытие предыдущей формы
+            }
+            else
+            {
+                MessageBox.Show("Введите имя");
+            }
+
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            if (textBox1.Text != "")
+            {
+                ScoreBoard.me = textBox1.Text;
+                DifficultyAnimals da = new DifficultyAnimals();
+                da.Show();//переход на форму
+                this.Hide();//закрытие предыдущей формы
+            }
+            else
+            {
+                MessageBox.Show("Введите имя");
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            if (textBox1.Text != "")
+            {
+                ScoreBoard.me = textBox1.Text;
+                DifficultyCities dcities = new DifficultyCities();
+                dcities.Show();//переход на форму
+                this.Hide();//закрытие предыдущей формы
+            }
+            else
+            {
+                MessageBox.Show("Введите имя");
+            }
+        }
+
+        private void saveName()
+        {
+            // Create a simple setting
+
 
         }
 
