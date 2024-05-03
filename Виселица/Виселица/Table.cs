f0302fc178b9742fb9f225c5c88b05a973fe951d
Sokolova Veronika Alekseevna <veronika.sock@yandex.ru>
2024-05-03 19:34:00 +0300

    [RP-2] Разработка класса Table

--- Виселица/Виселица/Table.cs
+++ Виселица/Виселица/Table.cs
@@ -11,6 +11,7 @@ namespace Виселица
 {
     public partial class Table : Form
     {
+        Dictionary<string, int> scores;
 
         public Table()
         {
@@ -19,11 +20,24 @@ namespace Виселица
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            Menu me = new Menu();
+            me.Show();//переход на форму
+            this.Hide();//закрытие предыдущей формы
         }
 
         private void Table_Load(object sender, EventArgs e)
         {
+            scores = ScoreBoard.get();
+            scores = scores.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            dataGridView1.ColumnCount = 2;
+            dataGridView1.RowCount = scores.Count;
+            int i = 0;
+            foreach (KeyValuePair<string, int> score in scores)
+            {
+                dataGridView1[0, i].Value = score.Key;
+                dataGridView1[1, i].Value = score.Value.ToString();
+                i++;
+            }
 
         }
     }
